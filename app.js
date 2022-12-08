fb36d5095d9024fd4de0ee5adc4be3ac28bde28e
HazukiMiki <xzhou92@gmail.com>
2022-12-08 11:00:05 -0500

    add local storage

--- app.js
+++ app.js
@@ -1,53 +1,38 @@
+// toggle darkmode
+let darkMode = localStorage.getItem("darkMode")
+const darkModeToggle = document.querySelector(".toggle-darklight")
 
-let darkModeState = false;
-// https://www.ditdot.hr/en/dark-mode-website-tutorial
-// Want to redo the local storage (relearn it) so I can make other changes
+const enableDarkMode = () => {
+    document.body.classList.add("dark-mode")
+    darkModeToggle.setAttribute("aria-label", "Switch to light mode")
+    document.querySelector("[toggle-img]").setAttribute("src", "images/sun.svg")
+    localStorage.setItem("darkMode", "enabled")
 
-
-
-
-//darklight toggle
-const useDark = window.matchMedia("(prefers-color-scheme: dark)");
-const darklightToggle = document.querySelector(".toggle-darklight")
-
-function toggleDarkMode(state) {
-    document.documentElement.classList.toggle("dark-mode", state);
-    darkModeState = state;
 }
-
-function setDarkModeLocalStorage(state) {
-    localStorage.setItem("dark-mode", state);
+const disableDarkMode = () => {
+    document.body.classList.remove("dark-mode")
+    darkModeToggle.setAttribute("aria-label", "Switch to dark mode")
+    document.querySelector("[toggle-img]").setAttribute("src", "images/moon.svg")
+    localStorage.setItem("darkMode", "disable")
 }
+darkMode === "enabled" ? enableDarkMode() : disableDarkMode()
 
-toggleDarkMode(localStorage.getItem("dark-mode") == "true");
+darkModeToggle.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode")
 
-useDark.addEventListener("change", (event) => {
-    toggleDarkMode(event.matches)
-})
-
-darklightToggle.addEventListener("click", setDarkTheme)
-function setDarkTheme() {
-    if (darkModeState) {
-        toggleDarkMode(false);
-        setDarkModeLocalStorage(false);
-        // note to self move this to local storage 
-        darklightToggle.setAttribute("aria-label", "Switch to dark mode")
-        document.querySelector("[toggle-img]").setAttribute("src", "images/moon.svg")
+    if (darkMode !== "enabled") {
+        enableDarkMode()
     } else {
-        toggleDarkMode(true);
-        setDarkModeLocalStorage(true);
-        // note to self move this to local storage 
-        darklightToggle.setAttribute("aria-label", "Switch to light mode")
-        document.querySelector("[toggle-img]").setAttribute("src", "images/sun.svg")
+        disableDarkMode()
     }
-}
+})
 
 // nav toggle
 const navMenuToggle = document.querySelector(".nav-menu-toggle")
 const navMenuClose = document.querySelector(".nav-menu-close")
 const navwrapper = document.querySelector(".nav-wrapper")
 
-// very temping to just make it 1 button and have it z-index above everthing ._.
+// very temping to just make it 1 button 
 navMenuToggle.addEventListener("click", toggleMenu)
 navMenuClose.addEventListener("click", toggleMenu)
 
